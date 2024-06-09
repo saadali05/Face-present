6dc30e2dd54e3f1aec16b62b09fd3d1b7cecd96e
saadali05 <saadatherali1256@gmail.com>
2024-06-09 05:11:04 +0500

    changed

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import joblib
 from shutil import rmtree
+import time
 
 app = Flask(__name__)
 
-nimgs = 10
+nimgs = 5
 
 imgBackground = cv2.imread("bg.png")
 
@@ -69,7 +70,8 @@ def train_model():
         joblib.dump(knn, 'static/face_recognition_model.pkl')
     except ValueError as e:
         print("Error:", e)
-
+        
+    time.sleep(5)  # Added delay after training
 
 def extract_attendance():
     df = pd.read_csv(f'Attendance/Attendance-{datetoday}.csv')
@@ -89,6 +91,8 @@ def add_attendance(name):
     if int(userid) not in list(df['Roll']):
         with open(f'Attendance/Attendance-{datetoday}.csv', 'a') as f:
             f.write(f'\n{username},{userid},{current_time}')
+    
+    time.sleep(5)  # Added delay after training
 
 
 def getallusers():
@@ -128,7 +132,7 @@ def start():
         return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(),
                                datetoday2=datetoday2,
                                mess='Error: Unable to open camera.')
-    
+
     while ret:
         ret, frame = cap.read()
         if frame is None:
@@ -136,6 +140,15 @@ def start():
             return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(),
                                    datetoday2=datetoday2,
                                    mess='Error: Unable to capture frame.')
+        # Log the dimensions of the captured frame
+        print("Frame dimensions:", frame.shape)
+
+        if frame.shape[0] <= 0 or frame.shape[1] <= 0:
+            # Handle the case where the frame dimensions are invalid
+            return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(),
+                                   datetoday2=datetoday2,
+                                   mess='Error: Invalid frame dimensions.')
+
         if len(extract_faces(frame)) > 0:
             (x, y, w, h) = extract_faces(frame)[0]
             cv2.rectangle(frame, (x, y), (x + w, y + h), (86, 32, 251), 1)
@@ -163,45 +176,57 @@ def start():
     cap.release()
     cv2.destroyAllWindows()
     names, rolls, times, l = extract_attendance()
-    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(),
-                           datetoday2=datetoday2)
-
-
+    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(),datetoday2=datetoday2) 
+    
+    time.sleep(5)  # Added delay after training
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
-    newusername = request.form['newusername']
-    newuserid = request.form['newuserid']
-    userimagefolder = 'static/faces/' + newusername + '_' + str(newuserid)
-    if not os.path.isdir(userimagefolder):
-        os.makedirs(userimagefolder)
-    i, j = 0, 0
-    cap = cv2.VideoCapture(0)
-    while 1:
-        _, frame = cap.read()
-        faces = extract_faces(frame)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 20), 2)
-            cv2.putText(frame, f'Images Captured: {i}/{nimgs}', (30, 30),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 20), 2, cv2.LINE_AA)
-            if j % 5 == 0:
+    if request.method == 'POST':
+        newusername = request.form['newusername']
+        newuserid = request.form['newuserid']
+        userimagefolder = 'static/faces/' + newusername + '_' + str(newuserid)
+        if not os.path.isdir(userimagefolder):
+            os.makedirs(userimagefolder)
+        else:
+            return render_template('home.html', mess='User already exists. Please use a different ID.',
+                                   names=[], rolls=[], times=[], l=0, totalreg=totalreg(), datetoday2=datetoday2)
+
+        i = 0
+        cap = cv2.VideoCapture(0)
+        while i < nimgs:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            faces = extract_faces(frame)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 20), 2)
+                cv2.putText(frame, f'Images Captured: {i}/{nimgs}', (30, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 20), 2, cv2.LINE_AA)
                 name = newusername + '_' + str(i) + '.jpg'
                 cv2.imwrite(userimagefolder + '/' + name, frame[y:y + h, x:x + w])
                 i += 1
-            j += 1
-        if j == nimgs * 5:
-            break
-        cv2.imshow('Adding new User', frame)
-        if cv2.waitKey(1) == 27:
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-    print('Training Model')
-    train_model()  # Ensure model is trained after adding new user
-    names, rolls, times, l = extract_attendance()
-    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(),
-                           datetoday2=datetoday2)
+
+            cv2.imshow('Adding new User', frame)
+            if cv2.waitKey(1) == 27 or i >= nimgs:
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+        print('Training Model')
+        train_model()  # Ensure model is trained after adding new user
+
+        names, rolls, times, l = extract_attendance()
+        return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(),
+                               datetoday2=datetoday2)
+    else:
+        return redirect(url_for('home'))  # If not a POST request, redirect to the home page
+
+
+
 
 
 @app.route('/delete/<roll>', methods=['GET'])
